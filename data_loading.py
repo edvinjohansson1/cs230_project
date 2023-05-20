713f8c00ca80944d26175294a372c32c8b9b00fb
NegarMat <129251042+NegarMat@users.noreply.github.com>
2023-05-20 11:51:09 -0700

    Update data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -4,62 +4,96 @@ import itertools
 from collections import Counter
 import  json
 
-	# Parameters
-# ==================================================
 
-# Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .05, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("json_file", "/mnt/home/dunan/Learn/Class/CSE842/Data/electornic_0-3.json", "Data source for review.")
-tf.flags.DEFINE_string("word2vec_file", "/mnt/home/dunan/Learn/Class/CSE842/Data/glove_6B/glove.6B.100d.txt", "File path for word2vec")
-#tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-#tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
 
-# Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 100, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
-tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.5, "L2 regularization lambda (default: 0.0)")
 
-# Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
-tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+def load_json_raw_data(chunk_path, num_limit):
+    x_text = []
+    y=[]
+    count = [0, 0, 0]
+    with open(chunk_path, 'r') as json_chunk_file:
+        for lines in json_chunk_file:
+            # line_buf = json_chunk_file.readline()
+            line_jsonify = json.loads(lines)  # build a dictionary
 
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
+            helpful_list = line_jsonify['helpful']
+	
+            # if helpful_list[1] == 0 or helpful_list[0] == helpful_list[1] / 2:
+            if helpful_list[0] == helpful_list[1] / 2:
+                helpful_score = [0, 1, 0]  # Neutral
+                count[1] += 1
+                if count[1]>num_limit:
+                    continue
+            elif helpful_list[0] > helpful_list[1] / 2:
+                helpful_score = [0, 0, 1]  # helpful
+                count[2] += 1
+                if count[2]>num_limit:
+                    continue
+            elif helpful_list[0] < helpful_list[1] / 2:
+                helpful_score = [1, 0, 0]  # not helpful
+                count[0] += 1
+                if count[0]>num_limit:
+                    continue
+            x_text.append(str(line_jsonify['reviewText']))
+            y.append(helpful_score)
 
-# ==================================================
-# Load data
-print("Loading data...")
-#x_text, y = load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
-x_text, y = load_json_raw_data(FLAGS.json_file, 13800)
+        json_chunk_file.close()
+    # print(self.review_text_list[6])
+    # print(self.review_text_list[7])
+    print('Chunk loaded. Label count: ')
+    print(count)
+    return [x_text, np.array(y)]
 
-# Build vocabulary
-max_document_length = max([len(x.split(" ")) for x in x_text])
-vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
-x = np.array(list(vocab_processor.fit_transform(x_text)))
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
 
-# Randomly shuffle data
-np.random.seed(10)
-shuffle_indices = np.random.permutation(np.arange(len(y)))
-x_shuffled = x[shuffle_indices]
-y_shuffled = y[shuffle_indices]
 
-# Split train/test set
-# TODO: This is very crude, should use cross-validation
-dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
-x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
-y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
-print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
-print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
-
+def load_embedding_vectors_glove(vocabulary, filename, vector_size):
+    # load embedding_vectors from the glove
+    # initial matrix with random uniform
+    embedding_vectors = np.random.uniform(-0.25, 0.25, (len(vocabulary), vector_size))
+    f = open(filename)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], dtype="float32")
+        idx = vocabulary.get(word)
+        if idx != 0:
+            embedding_vectors[idx] = vector
+    f.close()
+    return embedding_vectors
